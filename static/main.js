5cbf06d2616c693d7acbc04d11e9076bca3bfec9
HisenZhang <993848217@qq.com>
2018-04-09 19:45:40 +0800

    api get_video logic bug fixed

--- static/main.js
+++ static/main.js
@@ -1,12 +1,12 @@
-const electron=require("electron");
-const app=electron.app;
-const BrowserWindow=electron.BrowserWindow;
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 console.log('main.js is running...')
 
 let win;
 
-function createWindow(){
+function createWindow() {
     win = new BrowserWindow({
         width: 800,
         height: 900,
@@ -16,22 +16,22 @@ function createWindow(){
     });
     win.setMenu(null);
     win.loadURL(`file://${__dirname}/index.html`);
-    win.on("closed",function(){
+    win.on("closed", function() {
         win = null;
     });
 
 }
 
-app.on("ready",createWindow);
+app.on("ready", createWindow);
 
-app.on("window-all-closed",function(){
-    if(process.platform!="darwin"){
+app.on("window-all-closed", function() {
+    if (process.platform != "darwin") {
         app.quit();
     }
 });
 
-app.on("activate",function(){
-    if(win===null){
+app.on("activate", function() {
+    if (win === null) {
         createWindow();
     }
 });
